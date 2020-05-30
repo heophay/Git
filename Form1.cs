d453b3aeb60418f964b1f0ab79837ddb36fe61c4
heophay <heophay0509@gmail.com>
2020-05-31 00:20:20 +0700

    add giao dien

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiaoDien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(panel1.Width.ToString());
+            if (panel1.Width == 200)
+            {
+                panel1.Width = 46;
+                panel4.Visible = false;
+            }
+            else
+            {
+                panel1.Width = 200;
+                panel4.Visible = true;
+            }
+        }
+    }
+}
