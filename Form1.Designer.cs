694dc6fcf70946f172a650eb42db46dbdd5db035
heophay <heophay0509@gmail.com>
2020-06-03 01:36:17 +0700

    addddđ

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sideBar1 = new DevComponents.DotNetBar.SideBar();
+            this.ribbonBarMergeContainer1 = new DevComponents.DotNetBar.RibbonBarMergeContainer();
+            this.ratingStar1 = new DevComponents.DotNetBar.Controls.RatingStar();
+            this.progressBarX1 = new DevComponents.DotNetBar.Controls.ProgressBarX();
+            this.panelEx1 = new DevComponents.DotNetBar.PanelEx();
             this.SuspendLayout();
             // 
             // textBox1
@@ -38,11 +43,91 @@
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
             // 
+            // sideBar1
+            // 
+            this.sideBar1.AccessibleRole = System.Windows.Forms.AccessibleRole.ToolBar;
+            this.sideBar1.BorderStyle = DevComponents.DotNetBar.eBorderType.None;
+            this.sideBar1.ExpandedPanel = null;
+            this.sideBar1.Location = new System.Drawing.Point(457, 298);
+            this.sideBar1.Name = "sideBar1";
+            this.sideBar1.Size = new System.Drawing.Size(75, 23);
+            this.sideBar1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.sideBar1.TabIndex = 1;
+            this.sideBar1.Text = "sideBar1";
+            // 
+            // ribbonBarMergeContainer1
+            // 
+            this.ribbonBarMergeContainer1.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.ribbonBarMergeContainer1.Location = new System.Drawing.Point(376, 203);
+            this.ribbonBarMergeContainer1.Name = "ribbonBarMergeContainer1";
+            this.ribbonBarMergeContainer1.Size = new System.Drawing.Size(200, 100);
+            // 
+            // 
+            // 
+            this.ribbonBarMergeContainer1.Style.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.ribbonBarMergeContainer1.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.ribbonBarMergeContainer1.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.ribbonBarMergeContainer1.TabIndex = 2;
+            this.ribbonBarMergeContainer1.Visible = false;
+            // 
+            // ratingStar1
+            // 
+            // 
+            // 
+            // 
+            this.ratingStar1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.ratingStar1.Location = new System.Drawing.Point(376, 153);
+            this.ratingStar1.Name = "ratingStar1";
+            this.ratingStar1.Size = new System.Drawing.Size(75, 23);
+            this.ratingStar1.TabIndex = 3;
+            this.ratingStar1.Text = "ratingStar1";
+            this.ratingStar1.TextColor = System.Drawing.Color.Empty;
+            // 
+            // progressBarX1
+            // 
+            // 
+            // 
+            // 
+            this.progressBarX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.progressBarX1.Location = new System.Drawing.Point(376, 80);
+            this.progressBarX1.Name = "progressBarX1";
+            this.progressBarX1.Size = new System.Drawing.Size(75, 23);
+            this.progressBarX1.TabIndex = 4;
+            this.progressBarX1.Text = "progressBarX1";
+            // 
+            // panelEx1
+            // 
+            this.panelEx1.CanvasColor = System.Drawing.SystemColors.Control;
+            this.panelEx1.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.panelEx1.Location = new System.Drawing.Point(93, 91);
+            this.panelEx1.Name = "panelEx1";
+            this.panelEx1.Size = new System.Drawing.Size(200, 100);
+            this.panelEx1.Style.Alignment = System.Drawing.StringAlignment.Center;
+            this.panelEx1.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.panelEx1.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.panelEx1.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
+            this.panelEx1.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.panelEx1.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.panelEx1.Style.GradientAngle = 90;
+            this.panelEx1.TabIndex = 5;
+            this.panelEx1.Text = "panelEx1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelEx1);
+            this.Controls.Add(this.progressBarX1);
+            this.Controls.Add(this.ratingStar1);
+            this.Controls.Add(this.ribbonBarMergeContainer1);
+            this.Controls.Add(this.sideBar1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -54,5 +139,10 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private DevComponents.DotNetBar.SideBar sideBar1;
+        private DevComponents.DotNetBar.RibbonBarMergeContainer ribbonBarMergeContainer1;
+        private DevComponents.DotNetBar.Controls.RatingStar ratingStar1;
+        private DevComponents.DotNetBar.Controls.ProgressBarX progressBarX1;
+        private DevComponents.DotNetBar.PanelEx panelEx1;
     }
 }
